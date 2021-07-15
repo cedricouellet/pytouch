32fa26dfce3da72873ba5de727320957a1af0ca7
Cedric Ouellet <1925921@cstjean.qc.ca>
2021-07-14 23:18:02 -0400

    project done?

---
+++ pytouch-runner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+"""Convenience wrapper for running pytouch directly from source tree."""
+
+
+from pytouch.pytouch import main
+
+
+if __name__ == '__main__':
+    main()
